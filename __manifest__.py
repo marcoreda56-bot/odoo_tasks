891487b6fefcd01afdf695d60b6c45114b6695c8
marco <marcoreda56@gmail.com>
2026-05-25 14:08:04 +0300

    lab4

--- __manifest__.py
+++ __manifest__.py
@@ -3,11 +3,13 @@
     'author': 'Marco Reda',
     'depends': ['base', 'crm'],
     'data': [
+        'security/security.xml',         
         'security/ir.model.access.csv',
         'views/department_view.xml',
         'views/doctor_view.xml',
         'views/patient_view.xml',
         'views/customer_view.xml',
+        'report/patient_report.xml',      
     ],
     'installable': True,
     'application': True,
